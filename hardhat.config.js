18e28430984e4f0bfa7881923ac2f1a068b5aa66
Richard Noonan <noonanri@tcd.ie>
2022-08-23 15:06:34 +0400

    tidy up hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,15 +11,9 @@ require("hardhat-deploy")
  */
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const KOVAN_RPC_URL =
-    process.env.KOVAN_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL ||
-    "https://eth-rinkeby.alchemyapi.io/v2/X-7xxKeoJvokHnL-6TOEtkHFDp3vtEoj"
-const PRIVATE_KEY =
-    process.env.PRIVATE_KEY ||
-    "0x4099d067cacf18638e71c22acd685be7c284c0a21974368fd8f25e0cd58eb66d"
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || ""
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
